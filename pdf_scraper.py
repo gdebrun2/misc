231f8552f99c36bcb197f25fd7b7eb81638810eb
gdebrun2 <gdebrun2@illinois.edu>
2024-03-09 16:20:09 -0600

    adding useful functions

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -6,33 +6,29 @@ import matplotlib.pyplot as plt
 
 ### LG Spechsheet
 
-path = "C:\\Users\gtdebru\Documents\ModuleSpecSheets\LG\LG-NeON-R-360-375-specsheet.pdf"
+path = "~\Documents\ModuleSpecSheets\LG\LG-NeON-R-360-375-specsheet.pdf"
 
-#Variables to scrape: VOC, ISC, cell type
-patterns = [r'voc.*?\d+\.*\d*', r'isc.*?\d+\.*\d*']
-
+# Variables to scrape: VOC, ISC, cell type
+patterns = [r"voc.*?\d+\.*\d*", r"isc.*?\d+\.*\d*"]
 
 
 with pdfplumber.open(path) as pdf:
-    i = 0 
+    i = 0
     for page in pdf.pages:
 
         im = page.to_image(resolution=300)
 
-        table_settings = {"vertical_strategy": "text", 'horizontal_strategy':'lines_strict','min_words_vertical':3}
+        table_settings = {
+            "vertical_strategy": "text",
+            "horizontal_strategy": "lines_strict",
+            "min_words_vertical": 3,
+        }
 
         im.debug_tablefinder(table_settings)
 
-
-        im.save('C:\\Users\gtdebru\Documents\\' + str(i) + '.png',format = 'PNG')
+        im.save("~\Documents\\" + str(i) + ".png", format="PNG")
 
         i += 1
 
 
-
-
-
-print('done')
-
-
-
+print("done")
